e6357d55351e09a6bc883b1425963ccf6931ec61
AdamJednacz <adamjednacz4@gmail.com>
2024-08-23 20:33:11 +0200

    rental

--- my-app/src/components/Rental.js
+++ my-app/src/components/Rental.js
@@ -1,52 +1,46 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import break1 from "../assets/break.png";
-import motor from  "../assets/motor.png";
+import motor from "../assets/motor.png";
 import tools from "../assets/tools.png";
 import wheel from "../assets/wheel.png";
-import left from  "../assets/left_arrow.png";
-import right  from  "../assets/right_arrow.png";
+import left from "../assets/left_arrow.png";
+import right from "../assets/right_arrow.png";
 import cart from "../assets/cart.png";
 import car from "../assets/3d_car_1.png";
+import calendar from "../assets/calendar.png";
 
 const slides = [
-    { id: 1, content: <p>1</p> },
-    { id: 2, content: <p>2</p>},
-    { id: 3, content: <p>3</p> },
-    { id: 4, content: <p>4</p> },
-    { id: 5, content: <p>5</p> }
+    {id: 1, content: <p>1</p>},
+    {id: 2, content: <p>2</p>},
+    {id: 3, content: <p>3</p>},
+    {id: 4, content: <p>4</p>},
+    {id: 5, content: <p>5</p>}
 ];
 
 const availabilitySlides = [
-    { id: 1, content: <p>Days</p> },
-    { id: 2, content: <p>Hours</p> },
+    {id: 1, content: <p>Days</p>},
+    {id: 2, content: <p>Hours</p>},
 ];
 
 const Rental = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const [currentAvailabilitySlide, setCurrentAvailabilitySlide] = useState(0);
+    const slideCount = slides.length;
 
     const goToPrevious = () => {
-        const isFirstSlide = currentSlide === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentSlide - 1;
-        setCurrentSlide(newIndex);
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? slideCount - 1 : prevSlide - 1));
     };
 
     const goToNext = () => {
-        const isLastSlide = currentSlide === slides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentSlide + 1;
-        setCurrentSlide(newIndex);
+        setCurrentSlide((prevSlide) => (prevSlide === slideCount - 1 ? 0 : prevSlide + 1));
     };
 
     const goToPreviousAvailability = () => {
-        const isFirstSlide = currentAvailabilitySlide === 0;
-        const newIndex = isFirstSlide ? availabilitySlides.length - 1 : currentAvailabilitySlide - 1;
-        setCurrentAvailabilitySlide(newIndex);
+        setCurrentAvailabilitySlide((prevSlide) => (prevSlide === 0 ? availabilitySlides.length - 1 : prevSlide - 1));
     };
 
     const goToNextAvailability = () => {
-        const isLastSlide = currentAvailabilitySlide === availabilitySlides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentAvailabilitySlide + 1;
-        setCurrentAvailabilitySlide(newIndex);
+        setCurrentAvailabilitySlide((prevSlide) => (prevSlide === availabilitySlides.length - 1 ? 0 : prevSlide + 1));
     };
 
     return (
@@ -54,8 +48,15 @@ const Rental = () => {
             <div className="container">
                 <div className="slider">
                     <img src={left} alt="left arrow" onClick={goToPrevious}/>
-                    <div className="slide">
-                        {slides[currentSlide].content}
+                    <div className="slides">
+                        {slides.map((slide, index) => (
+                            <div
+                                className={`slide ${index === currentSlide ? 'active' : ''}`}
+                                key={slide.id}
+                            >
+                                {slide.content}
+                            </div>
+                        ))}
                     </div>
                     <img src={right} alt="right arrow" onClick={goToNext}/>
                 </div>
@@ -90,8 +91,14 @@ const Rental = () => {
                         <img src={right} alt="right arrow" onClick={goToNextAvailability}/>
                     </div>
                     <form>
-                        <input type="date" placeholder="date of receipt"/>
-                        <input type="date" placeholder="return date"/>
+                        <div className="input_container">
+                            <input type="date" placeholder="date of receipt"/>
+                            <img src={calendar} alt="calendar"/>
+                        </div>
+                        <div className="input_container">
+                            <input type="date" placeholder="return date"/>
+                            <img src={calendar} alt="calendar"/>
+                        </div>
                     </form>
 
                     <div className="result">
@@ -101,7 +108,7 @@ const Rental = () => {
                             <div className="result_box_amount">pole</div>
                         </div>
                     </div>
-                    <a>Here you can check full specification</a>
+                    <a>*Here you can check full specification</a>
                 </div>
                 <img className="car" src={car} alt="car"/>
             </div>
